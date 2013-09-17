83b5d0f87f96bf8c267188750903ea1ed70cfe81
Gabriel Vaquer <tujamaica@gmail.com>
2013-09-17 12:28:26 -0300

    Update QMatcher.js

--- QMatcher.js
+++ QMatcher.js
@@ -9,15 +9,11 @@
     }
 
     function QMatcher(source, search, wrapper) {
+        var results, matches, opts, rx, i, e;
         // Set the default wrapper tag
         wrapper = wrapper === false ? wrapper : typeof wrapper === 'string' ? wrapper : 'strong';
-
         // Transform the source into an iterable array
         source = !(source instanceof Array) ? [source] : source;
-
-        // Variable declaration
-        var results, matches, opts, rx, i, e;
-
         // Split search input into an array
         opts = search.split('');
         // Iterate over each character and scape it
@@ -25,31 +21,24 @@
             opts[i] = '(' + opts[i].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") + ')';
         }
 
-        // Build the regular expression
         rx = new RegExp(opts.join('(.*?)'), 'gi');
-        // We always return an array of matches
         results = [];
 
         function wrapMatch() {
             matches = [];
-            // Loop throught the matches and wrap them with a tag
             for (e = 1; e < arguments.length - 2; e++) {
                 // Check if the current argument is a match
                 matches[e] = (e % 2 === 1) ? ('<' + wrapper + '>' + arguments[e] + '</' + wrapper + '>') :  arguments[e];
             }
-            // Return the new wrapped match
             return matches.join('');
         }
 
-        // Iterate over each source
         for (i = 0; i < source.length; i++) {
             // Look for a match
             if (rx.test(source[i])) {
-                // Push the wrapped or the normal match
                 results.push((wrapper !== false) ? source[i].replace(rx, wrapMatch) : source[i]);
             }
         }
-
         return results;
     }
 
