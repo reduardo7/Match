739f29182c1053b8cee40105eb3eb2c8617e6286
Gabriel Vaquer <tujamaica@gmail.com>
2013-09-17 12:21:41 -0300

    Update matcher.js

--- matcher.js
+++ matcher.js
@@ -1,52 +1,67 @@
+/*jslint browser: true, plusplus: true, indent: 4 */
+
 (function (window) {
- 
+
     'use strict';
- 
-    function Matcher(source, search, tag) {
+
+    if (window.QMatcher !== undefined) {
+        return;
+    }
+
+    /**
+     * Looks for matches within an array and returns them wrapped in tags (optional)
+     * @author Gabriel Vaquer
+     * @param {Array|String} source
+     * @param {String} search
+     * @param {String|Bool} wrapper
+     * @return {Array}
+     */
+    function QMatcher(source, search, wrapper) {
         // Set the default wrapper tag
-        tag = tag === false ? tag :
-            typeof tag === 'string' ? tag : 'strong';
+        wrapper = wrapper === false ? wrapper : typeof wrapper === 'string' ? wrapper : 'strong';
+
         // Transform the source into an iterable array
         source = !(source instanceof Array) ? [source] : source;
- 
+
         // Variable declaration
-        var results, matches, tmpl, opts, rx, i, e;
- 
+        var results, matches, opts, rx, i, e;
+
         // Split search input into an array
         opts = search.split('');
         // Iterate over each character and scape it
         for (i = 0; i < opts.length; i++) {
             opts[i] = '(' + opts[i].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") + ')';
         }
+
         // Build the regular expression
         rx = new RegExp(opts.join('(.*?)'), 'gi');
- 
         // We always return an array of matches
         results = [];
+
+        function wrapMatch() {
+            matches = [];
+            // Loop throught the matches and wrap them with a tag
+            for (e = 1; e < arguments.length - 2; e++) {
+                // Check if the current argument is a match
+                matches[e] = (e % 2 === 1) ? ('<' + wrapper + '>' + arguments[e] + '</' + wrapper + '>') :  arguments[e];
+            }
+            // Return the new wrapped match
+            return matches.join('');
+        }
+
         // Iterate over each source
         for (i = 0; i < source.length; i++) {
             // Look for a match
             if (rx.test(source[i])) {
-                // Add the normal or wrapped match to the results array
-                results.push((tag !== false) ?
-                    source[i].replace(rx, function () {
- 
-                    matches = [];
-                    // Loop throught the matches and wrap them with a tag
-                    for (e = 1; e < arguments.length - 2; e++) {
-                        // Check if the current argument is a match
-                        matches[e] = (e % 2 === 1) ?
-                            ('<' + tag + '>' + arguments[e] + '</' + tag + '>') :  arguments[e];
-                    }
-                    // Return the new wrapped match
-                    return matches.join('');
-                }) : source[i]); // Else return the normal match
+                // Push the wrapped or the normal match
+                results.push((wrapper !== false) ? source[i].replace(rx, wrapMatch) : source[i]);
             }
         }
- 
+
         return results;
     }
- 
-    window.Matcher = Matcher;
- 
-}(window));
+
+    // Expose it globally
+    window.QMatcher = QMatcher;
+
+}(window || this));
