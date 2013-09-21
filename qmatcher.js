8f7f0df8681c5f34631b54ca59d663f2a6e6308a
Gabriel Vaquer <tujamaica@gmail.com>
2013-09-21 18:44:13 -0300

    Update qmatcher.js

--- qmatcher.js
+++ qmatcher.js
@@ -1,50 +1,30 @@
-/*jslint browser: true, plusplus: true, indent: 4 */
-
 (function (window) {
 
     'use strict';
 
-    if (window.QMatcher !== undefined) {
-        return;
-    }
-
     function QMatcher(source, search, wrapper) {
-        var results, opts, rx, i, e;
+        var results, matches, rx, i;
+        source = source instanceof Array ? source : [source];
 
-        // Set the default wrapper tag
-        wrapper = wrapper !== undefined ? wrapper : false;
-        // Transform the source into an iterable array
-        source = !(source instanceof Array) ? [source] : source;
-        // Split search input into an array
-        opts = search.split('');
+        rx = new RegExp(search.split('').map(function (item) {
+            return '(' + item.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/, "\\$&") + ')';
+        }).join('(.*?)'), 'gi');
 
-        // Iterate over each character and scape it
-        for (i = 0; i < opts.length; i++) {
-            opts[i] = '(' + opts[i].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/, "\\$&") + ')';
-        }
+        results = source.filter(function (item) {
+            return rx.test(item);
+        });
 
-        rx = new RegExp(opts.join('(.*?)'), 'gi');
-        results = [];
-
-        function wrapMatch() {
-            var matches = [];
-            for (e = 1; e < arguments.length - 2; e++) {
-                // Check if the current argument is a match
-                matches[e] = (e % 2 === 1) ? wrapper.replace(/\%s/, arguments[e]) : arguments[e];
-            }
-            return matches.join('');
-        }
-
-        for (i = 0; i < source.length; i++) {
-            // Look for a match
-            if (rx.test(source[i])) {
-                results.push((wrapper !== false) ? source[i].replace(rx, wrapMatch) : source[i]);
-            }
-        }
-        return results;
+        return wrapper !== undefined ? results.map(function (item) {
+            return item.replace(rx, function () {
+                matches = [];
+                for (i = 1; i < arguments.length - 2; i++) {
+                    matches[i] = (i % 2 === 1) ? wrapper.replace(/\%s/, arguments[i]) : arguments[i];
+                }
+                return matches.join('');
+            });
+        }) : results;
     }
 
-    // Expose it globally
     window.QMatcher = QMatcher;
 
 }(window));
