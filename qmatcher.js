dcb21ac32f7a4653f8fe0988317617670d99c9b2
Gabriel Vaquer <tujamaica@gmail.com>
2013-09-23 16:14:32 -0300

    Update qmatcher.js

--- qmatcher.js
+++ qmatcher.js
@@ -1,30 +1,51 @@
-(function (window) {
+(function ( window ) {
 
     'use strict';
 
-    function QMatcher(source, search, wrapper) {
-        var results, matches, rx, i;
-        source = source instanceof Array ? source : [source];
+    function QMatcher( source, search, wrapper ) {
+        var results, opts, rx, i, e;
 
-        rx = new RegExp(search.split('').map(function (item) {
-            return '(' + item.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/, "\\$&") + ')';
-        }).join('(.*?)'), 'gi');
+        // Transform the source into an iterable array
+        if ( ! (source instanceof Array ) ) {
+            source = [ source ];
+        }
 
-        results = source.filter(function (item) {
-            return rx.test(item);
-        });
+        opts = search.split( '' );
+        // Iterate over each character and scape it
+        for ( i = 0; i < opts.length; i++ ) {
+            opts[ i ] = '(' + opts[ i ].replace( /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/, "\\$&" ) + ')';
+        }
 
-        return wrapper !== undefined ? results.map(function (item) {
-            return item.replace(rx, function () {
-                matches = [];
-                for (i = 1; i < arguments.length - 2; i++) {
-                    matches.push((i % 2 === 1) ? wrapper.replace(/\%s/, arguments[i]) : arguments[i]);
+        rx = new RegExp( opts.join( '(.*?)' ), 'gi' );
+        results = [];
+
+        function wrapMatch() {
+            var matches = [];
+            for ( e = 1; e < arguments.length - 2; e++ ) {
+                // Check if the current argument is a match
+                matches.push( ( e % 2 === 1 ) ? wrapper.replace( /\%s/, arguments[ e ] ) : arguments[ e ] );
+            }
+            return matches.join( '' );
+        }
+
+        // This duplicated code is for performance only
+        if ( wrapper !== undefined ) {
+            for ( i = 0; i < source.length; i++ ) {
+                if ( rx.test( source[ i ] ) ) {
+                    results.push( source[ i ].replace( rx, wrapMatch ) );
                 }
-                return matches.join('');
-            });
-        }) : results;
+            }
+        } else {
+            for ( i = 0; i < source.length; i++ ) {
+                if ( rx.test( source[ i ] ) ) {
+                    results.push( source[ i ] );
+                }
+            }
+        }
+
+        return results;
     }
 
+    // Expose it globally
     window.QMatcher = QMatcher;
-
-}(window));
+})( window );
