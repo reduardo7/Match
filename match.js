8bbd972a734622fa966baff3e2ab0a7d8222c155
Gabriel Vaquer <tujamaica@gmail.com>
2013-12-09 12:11:01 -0200

    Update match.js

--- match.js
+++ match.js
@@ -1,42 +1,48 @@
 ;(function(window) {
   "use strict";
 
-  var rescape = /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/,
-      rgroup = /[a-zA-Z0-9]/gi;
+  var document = window.document;
 
-  function replaceWith(regexp, item, replace) {
-    var matches, results = [], i;
-    matches = item.match(regexp).slice(1);
-    for (i = 0; i < matches.length; i++) {
-      results.push((i % 2 === 1) ? replace.replace(/\%s/, matches[i]) : matches[i]);
+  function Match(source, search, template) {
+    var
+
+    results = [],
+    compile = (template != null && typeof template === "string"),
+    slength, current, clength, token, lindex, schar, i, e;
+
+    if (Object.prototype.toString.call(source) !== "[object Array]") {
+      source = [source];
     }
-    return results.join("");
-  }
+    slength = source.length;
+    i = slength >>> 0;
 
-  function Match(source, find, replace) {
-    var results = [], replace, r, i;
-    source = (Object.prototype.toString.call(source) === "[object Array]") ? source : [source]
-    find = find || "";
-    r = new RegExp(["(.*)",
-      find.replace(rescape, "(\\$&)")
-          .replace(rgroup, "($&)")
-          .replace(/(\(.+?\))/g, "$1(.*)")
-      ].join("")
-    );
-    if (typeof replace !== "string") {
-      for (i = 0; i < source.length; i++) {
-        if (r.test(source[i])) {
-          results.push(source[i]);
+    for (; i--;) {
+      current = source[i];
+      clength = current.length;
+      lindex = 0;
+      token = [];
+      for (e = 0; e < clength; e++) {
+        schar = current[e];
+        if (schar === search.charAt(lindex)){
+          token.push([ e, schar ]);
+          lindex++;
+        }
+      }
+      if (token.length === search.length) {
+        if (compile) {
+          current = current.split("");
+
+          for (e = 0; e < token.length; e++) {
+            current.splice(token[e][0], 1, template.replace(/\%s/, token[e][1]));
+          }
+          results.push(current.join(""));
+        }
+        else {
+          results.push(current);
         }
       }
     }
-    else {
-      for (i = 0; i < source.length; i++) {
-        if (r.test(source[i])) {
-          results.push(replaceWith(source[i]));
-        }
-      }
-    }
+
     return results;
   }
 
