44115a747d2e8ff8e575efaac031a974d33baee3
Gabriel Vaquer <tujamaica@gmail.com>
2013-12-09 16:30:23 -0200

    Update match.js

--- match.js
+++ match.js
@@ -1,19 +1,27 @@
 ;(function(window) {
   "use strict";
 
-  function Match(source, search, template) {
-    var
+  function replace(find, rep, orig) {
+    var indexOf = orig.indexOf(find);
+    return orig.slice(0, indexOf) + rep + orig.slice(indexOf + find.length);
+  }
 
+  function Match(source, search, options) {
+    options = options || {};
+    var
+    template = options.template,
+    caseSensitive = options.caseSensitive || false,
+    toLowerCase = (caseSensitive === true ? function(str) {
+      return str;
+    } : function(str) { return str.toLowerCase(); }),
     results = [],
     compile = (template != null && typeof template === "string"),
     slength, current, clength, token, lindex, schar, i, e;
-
-    if (Object.prototype.toString.call(source) !== "[object Array]") {
+    if (!(typeof source !== "string" && source.length != null)) {
       source = [source];
     }
     slength = source.length;
     i = slength >>> 0;
-
     for (; i--;) {
       current = source[i];
       clength = current.length;
@@ -21,7 +29,7 @@
       token = [];
       for (e = 0; e < clength; e++) {
         schar = current[e];
-        if (schar === search.charAt(lindex)){
+        if (toLowerCase(schar) === toLowerCase(search.charAt(lindex))) {
           token.push([ e, schar ]);
           lindex++;
         }
@@ -31,7 +39,7 @@
           current = current.split("");
 
           for (e = 0; e < token.length; e++) {
-            current.splice(token[e][0], 1, template.replace(/\%s/, token[e][1]));
+            current.splice(token[e][0], 1, replace("%s", token[e][1], template));
           }
           results.push(current.join(""));
         }
@@ -40,7 +48,6 @@
         }
       }
     }
-
     return results;
   }
 
